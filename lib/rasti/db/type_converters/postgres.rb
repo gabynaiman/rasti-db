bcb854ad7871558f1c945192c2acba05bc6a6b57
Eduardo Neira <eneira@keepcon.com>
2019-08-16 18:00:08 -0300

    Postgres Type Converter

--- lib/rasti/db/type_converters/postgres.rb
+++ lib/rasti/db/type_converters/postgres.rb
@@ -3,7 +3,7 @@ module Rasti
     module TypeConverters
       class Postgres
 
-        POSTGRES_CONVERTERS = [Postgres::JSON, Postgres::JSONB, Postgres::HStore, Postgres::Array]
+        CONVERTERS = [Postgres::JSON, Postgres::JSONB, Postgres::HStore, Postgres::Array]
 
         @to_db_mapping = {}
         @from_db_mapping = {}
@@ -11,17 +11,22 @@ module Rasti
         class << self
 
           def to_db(db, collection_name, attribute_name, value)
-            convertions = convertions_for(db, collection_name)
+            to_db_mapping = to_db_mapping_for db, collection_name
 
-            if convertions.key? attribute_name
-              convertions[attribute_name][:block].call value, convertions[attribute_name][:match] 
+            if to_db_mapping.key? attribute_name
+              to_db_mapping[attribute_name][:converter].to_db value: value, 
+                                                              sub_type: to_db_mapping[attribute_name][:sub_type] 
             else
               value
             end
           end
 
           def from_db(object)
-
+            if from_db_mapping.key? object.class
+              from_db_mapping[object.class].from_db object: object
+            else
+              object
+            end
           end
 
           private
@@ -31,14 +36,24 @@ module Rasti
               columns = Hash[db.schema(collection_name)]
 
               columns.each_with_object({}) do |(name, schema), hash| 
-                TO_DB_MAPPING.each do |type, convertion|
-                  match = type.match schema[:db_type]
+                CONVERTERS.each do |converter|
+                  unless hash.key? name
+                    match = converter.column_type_regex.match schema[:db_type]
 
-                  hash[name] ||= {match: match, block: convertion} if match
+                    hash[name] = {converter: converter, sub_type: match.captures.first} if match
+                  end
                 end
               end
             end
-          end 
+          end
+
+          def from_db_mapping
+            @from_db_mapping ||= begin
+              CONVERTERS.each_with_object({}) do |converter, result|
+                result[converter.db_class] = converter
+              end              
+            end
+          end
 
         end
 
