6d8f14a396f80983fa54dec61c4c6e02b1074e3e
Eduardo Neira <edun327@gmail.com>
2019-11-28 17:32:42 -0300

    Change to coverage helper to avoid specs and autogenerated code in metrics

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -2,4 +2,8 @@ require 'simplecov'
 require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
-SimpleCov.start
+
+SimpleCov.start do
+  add_filter 'lib/rasti/db/nql/syntax.rb'
+  add_filter 'spec/'
+end
