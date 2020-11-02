63bcb2b4737f3ea7408b140792e4dd00b2931bc9
dsenarruzza <diegosenarruzza@gmail.com>
2020-11-02 14:07:02 -0300

    replace module computed attributes to class computed attribute

---
+++ lib/rasti/db/computed_attribute.rb
@@ -0,0 +1,22 @@
+module Rasti
+  module DB
+    class ComputedAttribute
+
+      attr_reader :identifier
+
+      def initialize(identifier, &join)
+        @identifier = identifier
+        @join = join
+      end
+
+      def apply_join(dataset)
+        join ? join.call(dataset) : dataset
+      end
+
+      private
+
+      attr_reader :join
+
+    end
+  end
+end
