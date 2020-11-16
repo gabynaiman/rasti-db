9bb948a4c14ecc6de978ac532076ee179ce17d90
dsenarruzza <diegosenarruzza@gmail.com>
2020-11-16 16:01:32 -0300

    add comparisons for greather and less in  filter condition postgres

---
+++ lib/rasti/db/nql/filter_condition_strategies/comparisons/postgres/greather_than.rb
@@ -0,0 +1,25 @@
+module Rasti
+  module DB
+    module NQL
+      module FilterConditionStrategies
+        module PostgresComparisons
+          class GreatherThan < Base
+            class << self
+
+              def for_array(attribute, arguments)
+                raise Comparisons::TypedComparisonNotSupported.new '>', 'array'
+              end
+
+              private
+
+              def common_filter_method(attribute, argument)
+                attribute > argument
+              end
+
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/rasti/db/nql/filter_condition_strategies/comparisons/postgres/less_than_or_equal.rb
@@ -0,0 +1,25 @@
+module Rasti
+  module DB
+    module NQL
+      module FilterConditionStrategies
+        module PostgresComparisons
+          class LessThanOrEqual < Base
+            class << self
+
+              def for_array(attribute, arguments)
+                raise Comparisons::TypedComparisonNotSupported.new '<=', 'array'
+              end
+
+              private
+
+              def common_filter_method(attribute, argument)
+                attribute <= argument
+              end
+
+            end
+          end
+        end
+      end
+    end
+  end
+end
