88ab91309beffb94c307cc82a1ecae34c85c4268
Gabriel Naiman <gnaiman@keepcon.com>
2020-12-09 09:14:52 -0300

    Remove unused methods

--- lib/rasti/db/nql/nodes/comparisons/less_than_or_equal.rb
+++ lib/rasti/db/nql/nodes/comparisons/less_than_or_equal.rb
@@ -9,14 +9,6 @@ module Rasti
               DB.nql_filter_condition_strategy.filter_less_than_or_equal attribute.identifier(collection_class), argument
             end
 
-            def filter_basic_attribute(collection_class)
-              attribute.identifier(collection_class) <= argument.value
-            end
-
-            def filter_array_attribute(collection_class)
-              array_strategy.filter_less_than_or_equal attribute.identifier(collection_class), argument.value
-            end
-            
           end
         end
       end
