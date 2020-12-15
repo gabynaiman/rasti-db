ccd98542cc2a4bbf92d0ece81c624ba83e68bb44
dsenarruzza <diegosenarruzza@gmail.com>
2020-12-15 09:27:58 -0300

    Revert "add each pages method to get rows in blocks"
    This reverts commit 46010cdf4beca6bc3f94fc624be58a00a2763f67.

--- spec/minitest_helper.rb
+++ spec/minitest_helper.rb
@@ -133,8 +133,6 @@ class Minitest::Spec
   let :db do
     Sequel.connect(driver).tap do |db|
 
-      db.extension :pagination
-
       db.create_table :users do
         primary_key :id
         String :name, null: false, unique: true
